76d713a83360fc901494b3bf828bc3f0b561347c
redhunter7 <adlirafif42121@gmail.com>
2025-11-06 18:49:53 +0700

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,31 @@ module.exports = {
       secondary: '#262626',
       accent: '#EDEBD7',
     },
+    extend: {
+      borderWidth: {
+        '1': '1px',  // Default border-1
+        '3': '3px',  // Custom border-3
+        '6': '6px',  // Custom border-6
+        '8': '8px',  // Custom border-6
+        '10': '10px', // Custom border-10
+        '12': '12px', // Custom border-xs
+        'lg': '5px',   // Custom border-lg
+        'custom-width': '0.75rem', // Custom border-custom-width
+      },
+      keyframes: {
+        'spin-color': {
+          '0%': { borderColor: '#B3001B', transform: 'rotate(0deg)' },
+          '25%': { borderColor: '#EDEBD7' },
+          '50%': { borderColor: '#B3001B', transform: 'rotate(180deg)' },
+          '75%': { borderColor: '#EDEBD7' },
+          '100%': { borderColor: '#B3001B', transform: 'rotate(360deg)' },
+        }
+      },
+      animation: {
+        'spin-color': 'spin-color 3s infinite linear',
+        'spin': 'spin 3s infinite linear'
+      }
+    }
   },
   variants: {
     extend: {},

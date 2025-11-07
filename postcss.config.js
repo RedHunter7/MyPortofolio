f6cc50d3c78bc3476b2e9b19642c4b77500494ee
redhunter7 <adlirafif42121@gmail.com>
2025-11-07 13:56:51 +0700

    improve performance & SEO

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+const cssnano = require('cssnano')
+
 module.exports = {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+      cssnano: {
+        preset: 'default'
+      }
     }
   }
